a4bdc9a4fc1f785fa21f2e3893d479945eb41b73
root <root@node-1.msf.ucsf.edu>
2015-11-15 02:21:29 -0500

    Scripts for klimt remote node nfs mounting

---
+++ scripts/deploy_remote_node.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# get the path to the script directory
+CONFIGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONFIG=${CONFIGDIR}/config.sh
+echo Using configuration: ${CONFIG}
+. ${CONFIG}
+
+# create node, mounting klimt /exports directly
+echo -n "pp_node: "
+docker run --name pp_node \
+--privileged \
+-v /exports/data3/seqdb:/prospector_seqdb \
+-v /exports/data3/params:/prospector_params \
+-v /exports/data:/prospector_data \
+-v /exports/prospector/repository:/prospector_data/repository \
+-e PP_DB_PORT_3306_TCP_ADDR=169.230.19.249 \
+-d prospector-node /btag_start.sh
+
